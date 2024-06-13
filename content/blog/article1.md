39907680c9716d8fbc5de1048c8eb49d0c68e2b9
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-06-13 22:23:51 +0200

    dossier assert et draft

--- content/blog/article1.md
+++ content/blog/article1.md
@@ -1,6 +1,11 @@
 ---
-title: 'Ma page Hello World'
-description: 'meta description de la page'
+title: 'Article 1' #titre de la page
+description: 'Un article qui permet...' #description dela page
+tags: ["programmation, "tech", "JS"]
+slug: 'mon-article-1'
+thumbnail: 'img/pexels-ander-garcia-1317358711-25016476.jpg'
+draft: false
+# date: '2024-06-13'
 ---
 
 # Article 1
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <ContentList path="/blog">
-       
+       <template #default="{list}">
+        <!-- <p>{{ list }}</p> -->
+        <div v-for="(blog, index) in list" :key="index" class="bg-white shodow-md round-2xl">
+          <h2 class="font-bold text-2xl">{{ blog.title }}</h2>
+          <p v-if="blog.description">{{ blog.description }}</p>
+          <p>{{ blog.date }}</p>
+        </div>
+      
+       </template>
+
+       <template #not-found>
+        <p>Aucun article trouvé</p>
+       </template>
     </ContentList>
   </div>
 </template>
 
 <script lang="ts" setup>
+import {  } from "module";
 
 
 </script>
