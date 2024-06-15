04e95e68363cf5d178eac7e9cb7bea9694726afa
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-06-15 15:52:16 +0200

    point d`arret

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <header>
-      <ul class="felx my-8 gap-2">
+      <ul class=" my-8 gap-2">
         <li><NuxtLink to="/">Accueil</NuxtLink></li>
         <li><NuxtLink to="/blog">Voir mon blog</NuxtLink></li>
+        <li><NuxtLink to="/blog/benin">Bénin</NuxtLink></li>
+        <li><NuxtLink to="">Politique</NuxtLink></li>
+        <li><NuxtLink to="">Culture</NuxtLink></li>
+        <li><NuxtLink to="">Litérature</NuxtLink></li>
+        <li><NuxtLink to="">nternational</NuxtLink></li>
+        <li><NuxtLink to="">Transport</NuxtLink></li>
+        <li><NuxtLink to="">Sport</NuxtLink></li>
       </ul>
-    </header>
+   </header>
     <slot />
   </div>
 </template>
