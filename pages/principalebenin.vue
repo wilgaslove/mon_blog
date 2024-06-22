cd425b176723063584be35a7759972f6f87bb21e
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-06-22 16:00:56 +0200

    annimation highfive

--- pages/principalebenin.vue
+++ pages/principalebenin.vue
@@ -63,7 +63,19 @@
    </div>
 
 
+
+   <div class="relative">
+    
+    <div class=" animate-pulse fixed bottom-[40%] right-2  bg-[#bbb]  text-lg text-white py-5 px-4 shadow-md h-[45%] w-[15%] ">
+      <img src="assets/img/highfive.png" alt="" class="object-cover bg[#bbb]" >
+      <h1 class="text-2xl font-bold text-blue-600/100 p-4">Highfive University</h1>
+      <P class="text-black font-bold" >
+        Découvrez l'école de codage <a href="#" class="text-blue-600/100 ">Highfive University</a> ... 
+      </P>
+    </div>
+   </div>
   <FooterBenin />
+
 </template>
 
 <script lang="ts" setup>
