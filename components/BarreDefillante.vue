f891b3af43f81c0ef2e7050dd194e687af91c4f6
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-06-24 14:49:37 +0200

    barre publicitaire mise en forme

--- components/BarreDefillante.vue
+++ components/BarreDefillante.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-    <marquee behavior="scroll" direction="left" class="text-white bg-black marquee fixed top-[7%] left-0 right-0 py-4 bg-black text-lg text-white shadow-md">
-      Ce texte défile vers la gauche
-   </marquee>
+    <marquee behavior="scroll" direction="bottom" class="text-black marquee fixed top-[7%] left-0 right-0 py-4 text-lg shadow-md font-bold" style="width: 100vw;">
+      <p>Ce texte défile vers la gauche Bonjour Highfive</p>
+      <p>Dr Love en promotion sur Pc neuf !</p>
+      <p>La rentrée scolaire à Highfive University est pour le 30 Septembre 2024 !</p>
+      <p>Vente flash des Ordinateurs venus à l'état neuf chez Le Soldat-du-Christ du 1er au 15 Juillet 2024 Contact Appel Whatsapp: (+229) 96-74-05-12  95-47-92-36</p>
+      <p>Dr Love en promotion sur Pc neuf !</p>
+      <p>Dr Love en promotion sur Pc neuf !</p>
+    </marquee>
   </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
-
 <style>
-.marquee {
-
+/* Affiche les paragraphes en ligne */
+marquee p {
+  display: inline-block;
+  margin-right: 4rem;
 }
 </style>
