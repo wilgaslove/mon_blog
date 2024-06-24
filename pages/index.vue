b12a558a07e22d87e79f44316c176a38f4a6c32d
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-06-24 20:09:32 +0200

    interface index principale

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,116 @@
 <template>
   <div>
-    <h1 class="text-4xl font-bold text-center mt-[10%]">
-      <!-- Bienvenue sur mon blog 🚀👩‍🚀 -->
-    </h1>
+    <div class="flex flex-wrap mt-[18%]">
+      <div class="grid">
+        <a href="#">
+        <img src="assets\img\000_33QN69V-1.webp" alt="l'image du président renversé"  >
+        </a>
+        <p class=""><a href="https://www.france24.com/fr/afrique/20240614-niger-l-immunit%C3%A9-de-l-ex-pr%C3%A9sident-mohamed-bazoum-lev%C3%A9-par-la-cour-d-%C3%A9tat">Le président Bazoum a été renversé en août 2023.</a></p>
+      </div>
+
+      <a href="#">
+        <div class="grid">
+          <img src="assets\img\top-jobs-1200-13062024-1024x683.jpg" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\jad20230222-gfbenin-ouverture-1.webp" alt="l'image du président de la republique du bénin">
+          <p class=""><a href="#">Son excellence Patrice TALON dans l'élan de la révélation du Bénin au monde</a></p>
+        </div>
+      </a>
+
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+
+      <a href="">
+        <div class="grid">
+          <img src="assets\img\niger_leader.webp" alt="">
+          <p class=""><a href="#">La politique en France</a></p>
+        </div>
+      </a>
+    </div>
+   
+
   </div>
 
-
+  
   <NavBlog />
 
 </template>
@@ -17,6 +122,19 @@
 
 </script>
 
-<style>
-
+<style scoped>
+.grid {
+  /* background-color: ; */
+  height: 35%;
+  width: 35%;
+  border: 1rem;
+  border-radius: 1rem;
+  margin: 2rem;
+  box-shadow: 2rem;
+  background-color: white;
+}
+p {
+  margin-top: 4px;
+  margin-left: 2px;
+}
 </style>
