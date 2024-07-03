0f5b8dd1413275b9e81c19d5a8f456e6b52d0ca8
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-03 19:16:16 +0200

    index principal page au point sm

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,20 +10,11 @@ export default {
       }
     },
     screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+     'sm': {'min': '100px','max': '459px'},
+      'md': {'min': '460px', 'max': '768px'},
+      'lg': {'min': '769px', 'max': '1024px'},
+      'xl': {'min': '1025px', 'max': '1536px'},
+      '2xl': {'min': '1537px'},
     }
   },
   plugins: [
