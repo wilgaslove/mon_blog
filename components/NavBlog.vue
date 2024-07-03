0b4365ba8b7bf08f2f7def3b3c01405a0a7ae8cb
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-03 15:23:11 +0200

    navBlog scrollable

--- components/NavBlog.vue
+++ components/NavBlog.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="container">
     <header class="relative" v-if="!isBeninPage">
-      <ul class="header flex justify-between fixed top-0 left-0 right-0  bg-black text-lg text-white py-5 px-4 shadow-md herUl">
-        <li><NuxtLink to="/">Accueil</NuxtLink></li>
-        <li><NuxtLink to="/blog">Technologie & Sciences</NuxtLink></li>
-        <li><NuxtLink to="/politique">Politique</NuxtLink></li>
-        <li><NuxtLink to="/culture">Culture</NuxtLink></li>
-        <li><NuxtLink to="/literature">Litérature</NuxtLink></li>
-        <li><NuxtLink to="/histoire">Histore & Vie</NuxtLink></li>
-        <li><NuxtLink to="/amour">Amour & Conseil</NuxtLink></li>
-        <li><NuxtLink to="/sport">Sport</NuxtLink></li>
-        <li><NuxtLink to="/principalebenin">Bénin</NuxtLink></li>
-      </ul>
-   </header>
+      <div class="scroll-container">
+        <ul class="header flex justify-between fixed top-0 left-0 right-0 bg-black text-lg text-white py-5 px-4 shadow-md">
+          <li><NuxtLink to="/">Accueil</NuxtLink></li>
+          <li><NuxtLink to="/blog">Technologie & Sciences</NuxtLink></li>
+          <li><NuxtLink to="/politique">Politique</NuxtLink></li>
+          <li><NuxtLink to="/culture">Culture</NuxtLink></li>
+          <li><NuxtLink to="/literature">Litérature</NuxtLink></li>
+          <li><NuxtLink to="/histoire">Histore & Vie</NuxtLink></li>
+          <li><NuxtLink to="/amour">Amour & Conseil</NuxtLink></li>
+          <li><NuxtLink to="/sport">Sport</NuxtLink></li>
+          <li><NuxtLink to="/principalebenin">Bénin</NuxtLink></li>
+        </ul>
+      </div>
+    </header>
     <slot />
   </div>
-
 </template>
+
 
 <script lang="ts" setup>
 import { useRoute } from 'vue-router';
@@ -27,16 +29,25 @@ const isBeninPage = route.path.includes('/benin')
 </script>
 
 <style scoped>
-li{
+.scroll-container {
+  overflow-x: auto;
+  white-space: nowrap;
+}
+
+.header {
+  display: inline-flex;
+  /* Other styles */
+}
+
+li {
+  display: inline-block;
   border: 1px;
   border-radius: 12px;
   padding-inline: 1%;
   background-color: rgb(100, 100, 218);
 }
+
 li:hover {
-  /* transition-delay: 1ms; */
   background-color: rgb(91, 91, 247);
-  
 }
- 
 </style>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,25 @@ export default {
       animation: {
         'bounce-slow': 'bounce 7s infinite',
         'pulse-slow': 'pulse 10s infinite',
-        wiggle: 'wiggle 1s ease-in-out infinite',
+        'wiggle': 'wiggle 1s ease-in-out infinite',
       }
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [
     require("@tailwindcss/typography")
