e2cd9350d3b18b21a0a4aaedfcd6d434ae1ce7c9
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-05 13:29:35 +0200

    md index culture

--- pages/culture/index.vue
+++ pages/culture/index.vue
@@ -5,10 +5,13 @@
        <template #default="{list}">
         <div class="mt-[17%] mb-[20%]
         sm:mt-[35%] sm:mb-[30%]
+         md:my-[20%]
         ">
           <div v-for="(culture, index) in list" :key="index" class="shadow-md round-2xl mb-1 border-2 border-[#aaba] 
-          sm:w-[60%] sm:h-[20%]  sm:mx-auto">
-            <div class="h-[320px] sm:h-[150px]">
+          sm:w-[60%] sm:h-[20%]  sm:mx-auto
+          md:w-[55%] md:h-[20%]  md:mx-auto
+          ">
+            <div class="h-[320px] sm:h-[150px] md:h-[150px]">
               <img :src=" culture.thumbnail" alt="" class="w-full h-full object-cover">
             </div>
             <h2 class="font-bold text-2xl">
