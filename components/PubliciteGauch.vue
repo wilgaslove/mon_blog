82f0a7407a5e591219282d8ce806bf9e788465c1
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-05 13:42:27 +0200

    md les pub

--- components/PubliciteGauch.vue
+++ components/PubliciteGauch.vue
@@ -4,7 +4,7 @@
    
     <div class="animate-bounce-slow fixed top-[21%] left-5  bg-[#bbb] text-lg text-black py-5 px-4 shadow-md  h-[30%] w-[15%] 
     sm:w-[20%] sm:h-[17%] sm:px-0 sm:left-1 sm:text-xs sm:pt-0 sm:text-
-    md:w-[22%] md:h-[18%] md:px-0 md:left-1 md:text-xs md:pt-0 md:text-
+    md:w-[20%] md:h-[18%] md:px-0 md:left-1 md:text-xs md:pt-0 md:text-
     ">
       <img src="assets/img/pc_gamer.webp" alt="">
       <p class="sm:px-1 sm:bg-[#bbb]"
@@ -13,14 +13,17 @@
     </div>
    </div>
 
-   <div class=" animate-pulse-slow fixed top-[55%] left-2 text-lg  py-5   h-[20%] w-[21%] ">
+   <div class=" animate-pulse-slow fixed top-[55%] left-2 text-lg  py-5   h-[20%] w-[21%] 
+   md:w-[19%]
+   ">
       
       <div class="p-1 bg-[#aaaa] 
       sm:text-xs sm:px-0 sm:pt-0
+      md:text-xs md:px-0 md:pt-0 
       ">
         <img src="assets\img\wil.jpg" alt="" class="object-cover " >
         
-        <h1 class=" mdfont-bold text-blue-600/100 
+        <h1 class=" md:font-bold text-blue-600/100 
          ">Wilgas ADJOVI</h1>
       </div>
       
