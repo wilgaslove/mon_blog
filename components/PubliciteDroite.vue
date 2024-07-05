471eca7d56f07e18dc9fb4f41741f429bb1c72d0
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-05 15:31:00 +0200

    xl pub gauche et droite

--- components/PubliciteDroite.vue
+++ components/PubliciteDroite.vue
@@ -6,6 +6,7 @@
     sm:w-[20%] sm:h-[21%] sm:top-[17%] sm:px-0 sm:pt-0 sm:text-xs sm:
     md:w-[20%] md:h-[25%] md:top-[17%] md:px-0 md:pt-0 md:text-xs
     lg:w-[22%] lg:h-[30%] lg:top-[17%] lg:px-0 lg:pt-0 lg:text-xs lg:mx-auto
+    xl:w-[22%] xl:h-[30%] xl:top-[17%] xl:px-0 xl:pt-0 xl:text-xs xl:mx-[1.5%]
     ">
 
       <a href="https://highfiveuniversity.bj/"><img src="assets/img/highfive.png" alt="" class="object-cover bg[#bbb]" >
--- components/PubliciteGauch.vue
+++ components/PubliciteGauch.vue
@@ -2,30 +2,33 @@
   <div>
     <div class="relative">
    
-    <div class="animate-bounce-slow fixed top-[21%] left-5  bg-[#bbb] text-lg text-black py-5 px-4 shadow-md  h-[30%] w-[15%] 
+    <div class="animate-bounce-slow fixed top-[21%] left-5   text-lg text-black py-5 px-4 shadow-2xl  h-[30%] w-[15%] 
     sm:w-[20%] sm:h-[17%] sm:px-0 sm:left-1 sm:text-xs sm:pt-0 sm:text-
     md:w-[20%] md:h-[18%] md:px-0 md:left-1 md:text-xs md:pt-0 md:text-
     lg:w-[20%] lg:h-[20%] lg:px-0 lg:left-1 lg:text-xs lg:pt-0 lg:mx-auto
+    xl:w-[20%] xl:h-[20%] xl:px-0 xl:left-1 xl:text-xs xl:pt-0 xl:mx-[1.5%]
     ">
       <img src="assets/img/pc_gamer.webp" alt="">
-      <p class="sm:px-1 sm:bg-[#bbb]"
+      <p class="px-1 bg-white"
       >Promo spéciale bureautique gamer</p>
-      <p class="sm:px-1 font-bold sm:pb-4 sm:bg-[#bbb]">95-47-92-36</p>
+      <p class="pb-1 px-1 font-bold sm:pb-4 bg-white">95-47-92-36</p>
     </div>
    </div>
 
    <div class=" animate-pulse-slow fixed top-[55%] left-2 text-lg  py-5   h-[20%] w-[21%] 
    md:w-[19%]
+   xl:w-[15%]
    ">
       
-      <div class="p-1 bg-[#aaaa] 
+      <div class="p-1  
       sm:text-xs sm:px-0 sm:pt-0
       md:text-xs md:px-0 md:pt-0 
       lg:text-xs lg:px-0 lg:pt-0 lg:mx-auto
+      xl:text-xs xl:px-0 xl:pt-0 xl:mx-auto
       ">
         <img src="assets\img\wil.jpg" alt="" class="object-cover " >
         
-        <h1 class=" md:font-bold text-blue-600/100 
+        <h1 class="font-bold text-black bg-white
          ">Wilgas ADJOVI</h1>
       </div>
       
