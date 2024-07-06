9a98916901b4736f6c61c160bac4fac010b49759
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-06 14:29:39 +0200

    présentation

--- pages/presentation.vue
+++ pages/presentation.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <h1>Soldats du Christ</h1>
+
+    <img src="/assets/img/" alt="">
     <p>Une équipe de jeune intègre, dynamique passionnée de l'inforamatique, technologie, science, l'art, la culture, la rédation et la politique</p>
-    <p></p>
+    <p>Nous sommes une équipe  de plus de vingt (20) jeunes travaillant dans divers domaines et qui combine nos talents pour élargie nos sphères de connaissance avec le but précis d'être  modele pour les jeunes à travers le monde entiers</p>
+
+    <p>Nous avons à notre actif plusieurs sites, applications, blog, rédations,...</p>
+
     
   </div>
 </template>
