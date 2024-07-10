c1872040d2e4c17283febcb773114861f81e9bcb
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-10 17:00:59 +0200

    configuration de tailwind.config.ts pour la basculation du mode sombre

--- app.vue
+++ app.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="max-w-4xl px-4 mx-auto">
-    <NuxtLayout >
-      <NuxtPage />
-    </NuxtLayout>
+  <div class="bg-white dark:bg-black">
+    
+    <div class="max-w-4xl px-4 mx-auto">
+      <NuxtLayout >
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
   </div>
 </template>
 <style>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.{vue,js,ts}',
+    './pages/**/*.{vue,js,ts}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
   theme: {
     extend: {
       animation: {
         'bounce-slow': 'bounce 7s infinite',
         'pulse-slow': 'pulse 10s infinite',
-        'wiggle': 'wiggle 1s  infinite',
-      }
+        'wiggle': 'wiggle 1s infinite',
+      },
     },
     screens: {
-     'sm': {'min': '100px','max': '459px'},
+      'sm': {'min': '100px', 'max': '459px'},
       'md': {'min': '460px', 'max': '768px'},
       'lg': {'min': '769px', 'max': '1042px'},
       'xl': {'min': '1043px', 'max': '1299px'},
       '2xl': {'min': '1300px'},
-    }
+    },
   },
+  darkMode: 'class',
   plugins: [
-    require("@tailwindcss/typography")
-    
+    require('@tailwindcss/typography'),
   ],
-}
-
+};
