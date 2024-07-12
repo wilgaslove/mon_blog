704f493cc543eada6bb06660064d3ca7981d09e6
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-12 19:55:40 +0200

    blog page

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 
   <div class="sm:mt-[40%]  md:mt-[20%] 
   ">
-  <div class="flex flex-wrap mt-[1%] mb-[10%] sm:mb-[40%]
+  <div class="flex flex-wrap mt-[1%] mb-[10%] 
   lg:grid lg:grid-cols-2 lg:gap-0  lg:mt-[15%] lg:mx-[22%]
   xl:grid xl:grid-cols-2 xl:gap-0  xl:mt-[15%] xl:mx-[22%]
   2xl:grid 2xl:grid-cols-2 2xl:gap-0  2xl:mt-[15%] 2xl:mx-[22%]
@@ -12,10 +12,10 @@
     <div>
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
                 
             <a target="_blank" href="https://www.huffingtonpost.fr/international/video/joe-biden-atteint-de-parkinson-le-flou-de-la-maison-blanche-provoque-la-colere-des-journalistes_236650.html">
@@ -31,10 +31,10 @@
       <div>
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
                 
             <a target="_blank" href="https://www.france24.com/fr/afrique/20240614-niger-l-immunit%C3%A9-de-l-ex-pr%C3%A9sident-mohamed-bazoum-lev%C3%A9-par-la-cour-d-%C3%A9tat">
@@ -51,10 +51,10 @@
         <a target="_blank" href="https://theconversation.com/la-politique-etrangere-de-la-france-apres-les-legislatives-le-prix-du-chaos-233390">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\top-jobs-1200-13062024-1024x683.jpg" alt=""class="">
             <p
@@ -69,10 +69,10 @@
         <a target="_blank" href="https://www.youtube.com/watch?v=-XAIAfq1Wd8">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\jad20230222-gfbenin-ouverture-1.webp" alt="l'image du président de la republique du bénin"class="">
             <p>
@@ -89,10 +89,10 @@
         <a target="_blank" href="https://www.rfi.fr/fr/podcasts/l-accent-des-autres/20240615-migrations-le-canada-est-le-cinqui%C3%A8me-pays-qui-accueille-le-plus-de-migrants-dans-le-monde">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\CAN.jpg" alt=""class="">
             <p>
@@ -108,10 +108,10 @@
         <a target="_blank" href="https://actu.capital.fr/economie-politique/un-tribunal-russe-saisit-plusieurs-centaines-de-millions-deuros-appartenant-a-trois-banques-europeennes-1496948?utm_source=outbrain&utm_medium=cpc&utm_term=FR_France+24+fr+%28Audiovisuel+Ext%C3%A9rieur+de+la+France+SA%29_France+24+-+Afrique&utm_content=Un+tribunal+russe+saisit+plusieurs+centaines+de+millions+d%E2%80%99euros%E2%80%AFappar&utm_campaign=pmo_cap_article_desktop_desktop_enfant_geoloc_flux_cat_eco_politique_3&dicbo=v4-M5uKiYF-1081550216">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\picture.jpg" alt=""class="">
             <p>
@@ -127,10 +127,10 @@
         <a target="_blank" href="https://www.24haubenin.info/?Des-Camerounais-arretes-et-juges-a-la-CRIET">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\img-20221014-wa0084-16-c7e8f.jpg" alt=""class="">
             <p>
@@ -146,10 +146,10 @@
         <a target="_blank" href="https://www.youtube.com/watch?v=dylowtjlf8w">
           <div class=" 
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <div class=" ">
               <video class=" object-cover" controls >
@@ -169,10 +169,10 @@
         <a target="_blank" href="https://afrique.tv5monde.com/information/niger-orano-fait-un-pas-vers-lexploitation-dun-important-gisement-duranium">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\niger.jpg" alt=""class="">
             <p>
@@ -188,10 +188,10 @@
         <a target="_blank" href="https://afrique.tv5monde.com/information/depart-des-troupes-americaines-du-niger-washington-sous-estime-la-junte">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\field_media_image-1401748-AP20029218125250.jpg" alt=""class="">
             <p>
@@ -207,10 +207,10 @@
         <a target="_blank" href="https://presidence.bj/actualite/comptes-rendus/342/audience-palais-marina---patrice-talon-recoit-delegation-conference-grandes-chancelleries-afrique-francophone-france">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\837410799363001719421757.jpeg" alt=""class="">
             <p>
@@ -226,10 +226,10 @@
         <a target="_blank" href="https://information.tv5monde.com/direct/direct-proche-orient-echanges-de-tirs-la-frontiere-israelo-libanaise-2727772">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\AP24174621723313.webp" alt="des maisons démolie"class="">
             <p>
@@ -245,10 +245,10 @@
         <a target="_blank" href="https://information.tv5monde.com/international/prisons-77880-detenus-au-1er-juin-en-france-nouveau-record-2727760">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\detenus_france.webp" alt=""class="">
             <p>
@@ -264,10 +264,10 @@
         <a target="_blank" href="https://information.tv5monde.com/direct/direct-ukraine-la-guerre-naurait-pas-eu-lieu-si-les-etats-unis-avaient-un-president-respecte">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\AP24174568371232_0.webp" alt=""class="">
             <p>
@@ -283,10 +283,10 @@
         <a target="_blank" href="https://information.tv5monde.com/societe/justice-quel-sort-pour-lartiste-malienne-rokia-traore-detenue-rome-2727084">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\AFP-TV_20200313_ACE_FRA_EnfantsJustice_1PV6CT_fr.webp" alt="l'artiste malienne Rokia Traoré"class="">
             <p>
@@ -302,10 +302,10 @@
         <a target="_blank" href="https://information.tv5monde.com/afrique/florent-couao-zotti-les-autorites-nigeriennes-ont-besoin-de-se-creer-une-bete-noire-pour">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\Salon_du_livre_de_Paris_2011_-_Florent_Couao-Zotti_-_004.webp" alt=""class="">
             <p>
@@ -321,10 +321,10 @@
         <a target="_blank" href="https://information.tv5monde.com/international/drones-americains-en-mer-noire-la-russie-menace-lotan-dune-confrontation-directe">
           <div class="
           sm:w-[70%] sm:mx- sm:justify-center sm:mt-4 sm:bg-white
-          md:w-[55%] md:mx- md:justify-center md:mt-4 md:bg-white
-          lg:w-[90%] lg:mx- lg:justify-center lg:mt-4 lg:bg-white
-          xl:w-[95%] xl:mx- xl:justify-center xl:mt-4 xl:bg-white
-          2xl:w-[90%] 2xl:mx- 2xl:justify-center 2xl:mt-4 2xl:bg-white
+          md:w-[55%] md:mx-auto md:justify-center md:mt-4 md:bg-white
+          lg:w-[90%] lg:mx-auto lg:justify-center lg:mt-4 lg:bg-white
+          xl:w-[95%] xl:mx-auto xl:justify-center xl:mt-4 xl:bg-white
+          2xl:w-[90%] 2xl:mx-auto 2xl:justify-center 2xl:mt-4 2xl:bg-white
           ">
             <img src="assets\img\field_media_image-2843683-45475c5cc9e3681b9ba5a92d0a84896e9fafbdf9.webp" alt=""class="">
             <p>
