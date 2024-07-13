445eba12fd8c7d3cecc9c25346afc61f6fb8692e
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-13 15:53:39 +0200

    configuration de nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// import { defineNuxtConfig } from 'nuxt'
+
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      // Configuration publique
+      firebaseConfig: {
+        apiKey: "YOUR_API_KEY",
+        authDomain: "YOUR_AUTH_DOMAIN",
+        projectId: "YOUR_PROJECT_ID",
+        storageBucket: "YOUR_STORAGE_BUCKET",
+        messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+        appId: "YOUR_APP_ID",
+        measurementId: "YOUR_MEASUREMENT_ID"
+      }
     }
   },
   tailwindcss: {
@@ -18,13 +27,16 @@ export default defineNuxtConfig({
     "@vueuse/nuxt",
     "nuxt-marquee",
     "@nuxt/icon",
-    "@formkit/auto-animate",
-    // "@nuxtjs/vuetify",
+    "@formkit/auto-animate"
   ],
   components: [
     { path: '~/components', pathPrefix: false },
   ],
   plugins: [
-    { src: '~/plugins/vue-carousel.js', mode: 'client' }
-  ]
+    { src: '~/plugins/vue-carousel.js', mode: 'client' },
+    { src: '~/plugins/firebase.client.ts', mode: 'client' }
+  ],
+  nitro: {
+    preset: 'firebase'
+  }
 })
