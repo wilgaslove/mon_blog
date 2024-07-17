bef0fb0f4a2cb533aeca5ec38397d97e577fe692
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-17 13:11:56 +0200

    pub gauche

--- components/PubliciteGauch.vue
+++ components/PubliciteGauch.vue
@@ -7,7 +7,7 @@
     md:w-[20%] md:h-[18%] md:px-0 md:left-1 md:text-xs md:pt-0 md:top-[30%]
     lg:w-[20%] lg:h-[20%] lg:px-0 lg:left-1 lg:text-xs lg:pt-0 lg:mx-[1%] lg:top-[30%]
     xl:w-[18%] xl:h-[20%] xl:px-0 xl:left-1 xl:text-xs xl:pt-0 xl:mx-[6%] xl:top-[30%]
-    2xl:w-[18%] 2xl:h-[20%] 2xl:px-0 2xl:left-1 2xl:text-xs 2xl:pt-0 2xl:mx-[4%] 2xl:top-[30%]
+    2xl:w-[15%] 2xl:h-[20%] 2xl:px-0 2xl:left-1 2xl:text-xs 2xl:pt-0 2xl:ml-[12%] 2xl:top-[30%]
     ">
       <img src="assets/img/pc_gamer.webp" alt="">
       <p class=" bg-white"
@@ -21,7 +21,7 @@
    md:w-[19%]  lg:mx-[4%]
    lg:w-[15%]  xl:ml-[6%] 
    xl:w-[15%]  xl:ml-[6%] 
-   2xl:w-[15%] 2xl:ml-[6%]
+   2xl:w-[15%] 2xl:ml-[6%] 2xl:ml-[12%]
    ">
       
       <div class="p-1  
@@ -29,7 +29,7 @@
       md:text-xs md:px-0 md:pt-0 
       lg:text-xs lg:px-0 lg:pt-0 
       xl:text-xs xl:px-0 xl:pt-0 
-      2xl:text-xs 2xl:px-0 2xl:pt-0 
+      2xl:text-xs 2xl:px-0 2xl:pt-0  
       ">
         <a href="/presentation">
           <img src="assets\img\logo.png" alt="" class="object-cover " >
