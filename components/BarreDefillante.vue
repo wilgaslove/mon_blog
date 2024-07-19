4cfaa750b7bc8df4890956669b3b2c863c7ef1a4
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-19 22:20:18 +0200

    barre deffilante& highfive

--- components/BarreDefillante.vue
+++ components/BarreDefillante.vue
@@ -2,7 +2,7 @@
   <!-- Utilisation du module marquee de nuxt: npx nuxi@latest module add marquee -->
   <div class="barrdefillante">
     <marquee behavior="scroll" direction="left" class="text-black bg-white marquee fixed  left-0 right-0 py-4 text-lg  font-bold
-    sm:top-[7.5%]
+    sm:top-[8%]
     md:top-[7%]
     lg:top-[7%]
     xl:top-[7%]
--- components/PubliciteDroite.vue
+++ components/PubliciteDroite.vue
@@ -3,7 +3,7 @@
     
     
     <div class=" animate-pulse-slow fixed bottom-[35%] right-2   text-lg text-white py-5 px-4 shadow-md h-[30%] w-[15%] 
-    sm:w-[25%] sm:h-[16%] sm:top-[27%] sm:px-0 sm:pt-0 sm:text-xs sm:bg-white sm:left-[78%]
+    sm:w-[25%] sm:h-[16%] sm:top-[23%] sm:px-0 sm:pt-0 sm:text-xs sm:bg-white sm:left-[78%]
     md:w-[20%] md:h-[18%] md:top-[27%] md:px-0 md:pt-0 md:text-xs md:bg-white
     lg:w-[18%] lg:h-[20%] lg:top-[27%] lg:px-0 lg:pt-0 lg:text-xs lg:bg-white
     xl:w-[15%] xl:h-[22%] xl:top-[27%] xl:px-0 xl:pt-0 xl:text-xs xl:mx-[8%] xl:bg-white
