319cad18481bf5df490d47e60e7f6dbd00cf275b
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-20 15:03:35 +0200

    alignement des pub 2xl

--- components/PubliciteGauch.vue
+++ components/PubliciteGauch.vue
@@ -7,7 +7,7 @@
     md:w-[20%] md:h-[18%] md:px-0 md:left-1 md:text-xs md:pt-0 md:top-[30%]
     lg:w-[15%] lg:h-[20%] lg:px-0 lg:left-1 lg:text-xs lg:pt-0 lg:ml-[5%] lg:top-[30%]
     xl:w-[15%] xl:h-[20%] xl:px-0 xl:left-1 xl:text-xs xl:pt-0 xl:ml-[10%] xl:top-[30%]
-    2xl:w-[15%] 2xl:h-[20%] 2xl:px-0 2xl:left-1 2xl:text-xs 2xl:pt-0 2xl:ml-[12%] 2xl:top-[30%]
+    2xl:w-[15%] 2xl:h-[20%] 2xl:px-0 2xl:left-1 2xl:text-xs 2xl:pt-0 2xl:ml-[15%] 2xl:top-[30%]
     ">
       <img src="assets/img/pc_gamer.webp" alt="">
       <p class=" bg-white"
@@ -21,7 +21,7 @@
    md:w-[19%]  lg:mx-[4%]
    lg:w-[15%]  xl:ml-[6%] 
    xl:w-[15%]  xl:ml-[10%] 
-   2xl:w-[15%] 2xl:ml-[11%] 2xl:ml-[17%]
+   2xl:w-[15%] 2xl:ml-[11%] 2xl:ml-[15%]
    ">
       
       <div class="p-1  
