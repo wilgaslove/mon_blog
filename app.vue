42461268cbd6a5314230632340735fed051a07b8
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-24 17:34:01 +0200

    index principal

--- app.vue
+++ app.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <NuxtLayout >
-      <NuxtPage />
-    </NuxtLayout>
+  <div class="bg-white dark:bg-black">
+    
+    <div class="max-w-4xl px-4 mx-auto">
+      <NuxtLayout >
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
   </div>
 </template>
+<style>
+body{
+  @apply bg-gray-100;
+}
+</style>
