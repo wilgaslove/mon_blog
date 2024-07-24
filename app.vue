28d6305ab86ff8363cac222cb118401de738889e
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-24 12:45:45 +0200

    réinitialisation du projet

--- app.vue
+++ app.vue
@@ -1,15 +1,6 @@
 <template>
-  <div class="bg-white dark:bg-black">
-    
-    <div class="max-w-4xl px-4 mx-auto">
-      <NuxtLayout >
-        <NuxtPage />
-      </NuxtLayout>
-    </div>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
   </div>
 </template>
-<style>
-body{
-  @apply bg-gray-100;
-}
-</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,5 @@
-// import { defineNuxtConfig } from 'nuxt'
-
-// Configuration de Nuxt
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true, // Active le rendu côté serveur (SSR)
- 
-  tailwindcss: {
-    configPath: "tailwind.config.ts"
-  },
-  css: [
-    "/assets/css/custom.css"
-  ],
-  devtools: { enabled: true },
-  modules: [
-    "@nuxt/content",
-    "@nuxtjs/tailwindcss",
-    "@vueuse/nuxt",
-    "nuxt-marquee",
-    "@nuxt/icon",
-    "@formkit/auto-animate"
-  ],
-  // composants
-  // components: [
-  //   { path: '~/components', pathPrefix: false },
-  // ],
- 
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
 })
