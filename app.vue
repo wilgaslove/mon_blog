322abc1e23ba7df8f402a6886733ed679ff3cafb
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-27 15:14:05 +0200

    configuration et installation d`extension tailwindcc

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-white dark:bg-black">
+    
+    <div class="max-w-4xl px-4 mx-auto">
+      <NuxtLayout >
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
   </div>
 </template>
+<style>
+body{
+  @apply bg-gray-100;
+}
+
+</style>
