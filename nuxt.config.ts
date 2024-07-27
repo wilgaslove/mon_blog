a73de9676d0387912c44e2d30a999518d9414d56
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-27 15:20:12 +0200

    installation de npx nuxi@latest module add marquee

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   
   devtools: { enabled: true },
 
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "nuxt-marquee"],
 
   css: [
     '@/assets/css/tailwind.css',
